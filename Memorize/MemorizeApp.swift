0a8c3b2e29e011dd47268e0a1f96c825ece91841
wkk4 <ricky.kwong95@gmail.com>
2024-04-23 14:52:58 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ricky Kwong on 2024-04-23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
